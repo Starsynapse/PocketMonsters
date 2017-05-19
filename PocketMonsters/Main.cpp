a20aa0e1f7c00dd18dca607cd1db13d33bfbae8c
Eduardo <starsynapse@gmail.com>
2017-05-18 19:14:24 -0700

    Add project files.

---
+++ PocketMonsters/Main.cpp
@@ -0,0 +1,15 @@
+/*Project Name: PocketMonsters
+Author: Eduardo Zamora
+Date Created: 18 May 2017
+Date Edited: 18 May 2017*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
